e1ae66d61ddfa9790a11a414d703756a20dd4815
monodo <olivier.monod@ne.ch>
2015-11-27 17:08:20 +0100

    add params to config file

--- src/static/map/config/config.js
+++ src/static/map/config/config.js
@@ -13,6 +13,7 @@ mb.params = {
         'tileUrl': 'http://tile{1-5}-mobilite2030.ne.ch/tiles/',
         'resolutions': [100, 50, 20, 10, 5],
         'matrixIds': [0,1,2,3,4],
-        'extentCorrection': 10000 //[m]
+        'extentCorrection': 10000, //[m]
+        'selectableLayers': ['routes_projets_routiers', 'trains2016', 'projet_rer', 'mobilite_douce', 'nouvelles_gares', 'arrets2016', 'arrets2030']
     }
 };
